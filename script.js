15c1e807620bfc8b5d7a1869c2a981af645a6eae
Kephira <arielleahlonkoba@gmail.com>
2025-10-09 14:03:40 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const bar = document.querySelector("nav")
+let valeur = 0;
+window.addEventListener("scroll",()=>{
+     if (window.scrollY > valeur){
+      bar.style.top="-70px";
+    }
+     else{
+       bar.style.top = 0;
+     }
+ valeur = window.scrollY;
+})
