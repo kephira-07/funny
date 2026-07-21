1004f12fe82d1c852374989ce6cf75a2b2c57396
kephira-07 <arielleahlonkoba@gmail.com>
2026-07-21 00:14:06 +0000

    Refactor scroll event listener for navigation bar

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const bar = document.querySelector("nav")
+const bar = document.querySelector("nav");
 let valeur = 0;
-window.addEventListener("scroll",()=>{
-     if (window.scrollY > valeur){
-      bar.style.top="-70px";
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > valeur) {
+        bar.style.top = "-70px";
+    } else {
+        bar.style.top = "0";
     }
-     else{
-       bar.style.top = 0;
-     }
- valeur = window.scrollY;
-})
+    valeur = window.scrollY;
+});
